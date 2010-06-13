6a95fe075739b6851b5ff7c9cf6101766be532d0
Toomore Chiang <toomore0929@gmail.com>
2010-06-14 02:11:53 +0800

    fetch the csv file.

--- main.py
+++ main.py
@@ -5,14 +5,23 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import xmpp
 
-from google.appengine.api import urlfetch
+#from google.appengine.api import urlfetch
 
-import urllib2,md5,logging
+import urllib2,md5,logging,csv
 
 ############## webapp Models ###################
 class MainPage(webapp.RequestHandler):
   def get(self):
+    url = 'http://www.twse.com.tw/ch/trading/exchange/STOCK_DAY_AVG/STOCK_DAY_AVG2.php?STK_NO=2363&myear=2010&mmon=06&type=csv'
+    cc = urllib2.urlopen(url)
+    csv_read = csv.reader(cc)
+
     self.response.out.write('Go Ri Stock')
+    #csv_read.next
+
+    for i in csv_read:
+      print i
+    self.response.out.write('<br>%s' % type(csv_read))
 
 ############## main Models ###################
 def main():
