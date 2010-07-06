195917c08de6a9288eca45fd4df3f72e3c05dfd4
Toomore Chiang <toomore0929@gmail.com>
2010-07-06 14:09:16 +0800

    Add data date list. Change list_data return value.
    self.list_data return a dictionary type value.
     - 'getr': getr,
     - 'stock_name': stock_name,
     - 'data_date': data_date

--- goristock.py
+++ goristock.py
@@ -25,15 +25,27 @@ import urllib2, logging, csv, re
 
 class goristock(object):
 
-  def __init__(self,stock_no):
+  def __init__(self,stock_no,data_num = 75):
+    """
+    stock_no: stock no.
+    data_num: default fetch numbers.(Default is 75)
+    """
     self.raw_data = []
     self.stock_name = ''
     self.stock_no = stock_no
+    self.data_date = []
     starttime = 0
-    while len(self.raw_data) < 160:
-      self.csv_read = self.fetch_data(stock_no, datetime.today() - timedelta(days = 30 * starttime))
-      self.raw_data = self.list_data(self.csv_read) + self.raw_data
-      starttime += 1
+
+    try:
+      while len(self.raw_data) < data_num:
+        self.csv_read = self.fetch_data(stock_no, datetime.today() - timedelta(days = 30 * starttime))
+        result = self.list_data(self.csv_read)
+        self.raw_data = result['getr'] + self.raw_data
+        self.data_date = result['data_date'] + self.data_date
+        self.stock_name = result['stock_name']
+        starttime += 1
+    except:
+      logging.info('Data not enough! %s' % stock_no)
 
     logging.info('Fetch %s' % stock_no)
 
@@ -56,6 +68,7 @@ class goristock(object):
       return False
 
   def fetch_data(self, stock_no, nowdatetime):
+    """ Fetch data from twse.com.tw """
     url = 'http://www.twse.com.tw/ch/trading/exchange/STOCK_DAY/STOCK_DAY_print.php?genpage=genpage/Report%(year)d%(mon)02d/%(year)d%(mon)02d_F3_1_8_%(stock)s.php&type=csv' % {'year': nowdatetime.year, 'mon': nowdatetime.month,'stock': stock_no}
     print url
     logging.info(url)
@@ -64,36 +77,50 @@ class goristock(object):
     return csv_read
 
   def list_data(self, csv_read):
+    """ Put the data into the 'self.raw_data' """
     getr = []
+    getdate = []
     otherinfo = []
     for i in csv_read:
       if self.ckinv(i):
         print i
         getr.append(self.covstr(i[6]))
+        getdate.append(i[0])
       else:
         otherinfo.append(i[0])
 
-    self.stock_name = otherinfo[0].split(' ')[2].decode('big5').encode('utf-8')
+    stock_name = otherinfo[0].split(' ')[2].decode('big5').encode('utf-8')
+    data_date = getdate
+    return_value = {
+      'getr': getr,
+      'stock_name': stock_name,
+      'data_date': data_date
+    }
     print otherinfo
-    print self.stock_name
-    return getr
+    print stock_name
+    return return_value
 
   @property
   def num_data(self):
+    """ Number of data. """
     return len(self.raw_data)
 
   @property
   def sum_data(self):
+    """ Sum of data. """
     return sum(self.raw_data)
 
   @property
   def avg_data(self):
+    """ Average of data. """
     return float(self.sum_data/self.num_data)
 
   def MA(self,days):
+    """ Moving Average with days. """
     return float(sum(self.raw_data[-days:]) / days)
 
   def MAC(self,days):
+    """ Comparing yesterday is high or low. """
     yesterday = self.raw_data[:]
     yesterday.pop()
     yes_MA = float(sum(yesterday[-days:]) / days)
