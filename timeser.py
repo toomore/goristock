0385258d644e4ccffff2c8d1ca3a28dd8a1bca57
Toomore Chiang <toomore0929@gmail.com>
2010-07-30 00:41:30 +0800

    Change format for output useing.

--- timeser.py
+++ timeser.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 from goristock import goristock
 
+def oop(aa):
+  return ('%s %s %s %.2f %+.2f %s %s %s %s %+.2f %s %s %.2f' % (aa.stock_no, aa.stock_name, aa.data_date[-1], aa.raw_data[-1], aa.range_per, aa.MAC(3), aa.MAC(6), aa.MAC(18), aa.MAO(3,6)[1], aa.MAO(3,6)[0][1][-1], aa.MAO(3,6)[0][0], aa.RABC, aa.stock_vol[-1]/1000)).encode('utf-8')
+
 def timetest(no):
   a = goristock(no)
-  def oop(aa):
-    return '%s %s %s %.2f(%+.2f) %s%s%s %s %+.2f %s %s' % (aa.stock_no,aa.stock_name,aa.data_date[-1],aa.raw_data[-1],a.range_per,aa.MAC(3),aa.MAC(6),aa.MAC(18),aa.MAO(3,6)[1],aa.MAO(3,6)[0][1][-1],aa.MAO(3,6)[0][0], aa.RABC)
-
   while len(a.raw_data) > 19:
     if a.MAO(3,6)[1] == '↑'.decode('utf-8') and (a.MAO(3,6)[0][1][-1] < 0 or ( a.MAO(3,6)[0][1][-1] < 1 and a.MAO(3,6)[0][1][-1] > 0 and a.MAO(3,6)[0][1][-2] < 0 and a.MAO(3,6)[0][0] == 3)) and a.VOLMAX3:
       print 'buy-: ' + oop(a)
-    elif a.MAO(3,6)[1] == '↓'.decode('utf-8') and a.MAO(3,6)[0][1][-1] > 0 and a.MAO(3,6)[0][0] <= 3 and a.VOLMAX3:
+    elif a.MAO(3,6)[1] == '↓'.decode('utf-8') and a.MAO(3,6)[0][1][-1] > 0 and a.MAO(3,6)[0][0] <= 3:
       print 'sell: ' + oop(a)
     else:
       print '----: ' + oop(a)
@@ -26,6 +26,9 @@ def overall(goback = 0):
         a.goback(goback)
 
       if a.MAO(3,6)[1] == '↑'.decode('utf-8') and (a.MAO(3,6)[0][1][-1] < 0 or ( a.MAO(3,6)[0][1][-1] < 1 and a.MAO(3,6)[0][1][-1] > 0 and a.MAO(3,6)[0][1][-2] < 0 and a.MAO(3,6)[0][0] == 3)) and a.VOLMAX3 and a.stock_vol[-1] > 1000*1000 and a.raw_data[-1] > 10:
-        print a.Cmd_display
+        #print a.Cmd_display
+        print 'buy-: ' + oop(a)
+      elif a.MAO(3,6)[1] == '↓'.decode('utf-8') and a.MAO(3,6)[0][1][-1] > 0 and a.MAO(3,6)[0][0] <= 3:
+        print 'sell: ' + oop(a)
     except:
       print i
