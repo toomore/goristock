f986784d9e3bba565598d5a720379f0c63e3a7c4
Toomore Chiang <toomore0929@gmail.com>
2010-08-01 16:42:25 +0800

    Fix fetch no data in the first day of month.

--- goristock.py
+++ goristock.py
@@ -51,7 +51,15 @@ class goristock(object):
       while len(self.raw_data) < data_num:
         # start fetch data.
         self.csv_read = self.fetch_data(stock_no, datetime.today() - timedelta(days = 30 * starttime))
-        result = self.list_data(self.csv_read)
+        try:
+          result = self.list_data(self.csv_read)
+        except:
+          if starttime == 0:
+            starttime += 1
+            self.csv_read = self.fetch_data(stock_no, datetime.today() - timedelta(days = 30 * starttime))
+            result = self.list_data(self.csv_read)
+          logging.info('In first day of months %s' % stock_no)
+
         self.raw_data = result['stock_price'] + self.raw_data
         self.data_date = result['data_date'] + self.data_date
         self.stock_name = result['stock_name']
