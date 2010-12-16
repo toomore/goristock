9313e9e32d77674ec91c8c98d827c61780fbc0cb
Toomore Chiang <toomore0929@gmail.com>
2010-12-16 22:33:13 +0800

    Add /API/liststock, /API/searchstock.

--- goapi.py
+++ goapi.py
@@ -26,6 +26,7 @@ except:
 
 import goristock
 import realtime
+import twseno
 
 class goapi(object):
   def __init__(self, stock_no):
@@ -66,3 +67,25 @@ def weight():
   except:
     re = {'ERRORREPORT': "Can't fetch weight data."}
   return json.dumps(re)
+
+def stocklist():
+  stock_list = twseno.twseno().allstockno
+  re_st = {}
+  for i in stock_list:
+    re_st.update({i: stock_list[i].decode('utf-8')})
+  re = {'stocklist': re_st, 'n': len(stock_list)}
+  re.update({'last_update': twseno.twseno().last_update})
+  return json.dumps(re)
+
+def searchstock(q):
+  q = q.encode('utf-8').replace(' ','')
+  if q:
+    rq = twseno.twseno().search(q)
+    re_se ={}
+    for i in rq:
+      re_se.update({i: rq[i].decode('utf-8')})
+    re = {'result': re_se, 'n': len(rq)}
+    return json.dumps(re)
+  else:
+    re = {'ERRORREPORT': "No keyword."}
+    return json.dumps(re)
