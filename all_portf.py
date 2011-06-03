a3056f929216b95ac1169449d175e803e0422e12
Toomore Chiang <toomore0929@gmail.com>
2011-06-03 12:18:06 +0800

    Tiny change.

--- all_portf.py
+++ all_portf.py
@@ -26,17 +26,17 @@ class all_portf(object):
     self.a = a
 
   def ck_portf_001(self):
-    ''' 3-6負乖離且向上，三日內最大量，成交量大於1000，收盤價大於 10 '''
+    ''' 3-6負乖離且向上，三日內最大量，成交量大於 1000 張，收盤價大於 10 元。（較嚴謹的選股）'''
     return self.a.MAO(3,6)[1] == '↑'.decode('utf-8') and (self.a.MAO(3,6)[0][1][-1] < 0 or ( self.a.MAO(3,6)[0][1][-1] < 1 and self.a.MAO(3,6)[0][1][-1] > 0 and self.a.MAO(3,6)[0][1][-2] < 0 and  self.a.MAO(3,6)[0][0] == 3)) and self.a.VOLMAX3 and self.a.stock_vol[-1] > 1000*1000 and self.a.raw_data[-1] > 10
 
   def ck_portf_002(self):
-    ''' 3日均價大於6日均價，6日均價大於18日均價 '''
+    ''' 3日均價大於6日均價，6日均價大於18日均價。（短中長線呈現多頭的態勢） '''
     return self.a.MA(3) > self.a.MA(6) > self.a.MA(18)
 
   def ck_portf_003(self):
-    ''' 當日成交量，大於前三天的總成交量 '''
+    ''' 當日成交量，大於前三天的總成交量。（短線多空動能） '''
     return self.a.stock_vol[-1] > sum(self.a.stock_vol[-4:-1])
 
   def ck_portf_004(self):
-    ''' 價走平一個半月 '''
+    ''' 價走平一個半月。（箱型整理、盤整） '''
     return self.a.SD < 0.25
