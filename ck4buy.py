1b3d105109bbea8a3788d9f3a2abe1afd303cef3
Toomore Chiang <toomore0929@gmail.com>
2011-10-03 12:55:26 +0800

    [issues #31 #32] Build B4P model.

--- ck4buy.py
+++ ck4buy.py
@@ -4,20 +4,28 @@
 
 from grs import goristock
 from grs.twseno import twseno
-from grs.all_portf import all_portf
+from grs.all_portf import B4P
 
 for i in twseno().allstockno:
   a = goristock.goristock(i)
-  pa = all_portf(a)
+  pa = B4P(a)
   try:
     #a.goback(3)
-    if pa.ck_portf_006() and pa.ck_portf_007():
-      print a.Cmd_display,'量大收紅'
-    elif pa.ck_portf_006() and pa.ck_portf_008():
-      print a.Cmd_display,'量縮價不跌'
-    elif pa.ck_portf_006() and pa.ck_portf_009():
-      print a.Cmd_display,'三日均價由下往上'
-    elif pa.ck_portf_006() and pa.ck_portf_010():
-      print a.Cmd_display,'三日均價大於六日均價'
+    if pa.ckMinsGLI and pa.B1:
+      print 'O-', a.Cmd_display,'量大收紅'
+    elif pa.ckMinsGLI and pa.B2:
+      print 'O-', a.Cmd_display,'量縮價不跌'
+    elif pa.ckMinsGLI and pa.B3:
+      print 'O-', a.Cmd_display,'三日均價由下往上'
+    elif pa.ckMinsGLI and pa.B4:
+      print 'O-', a.Cmd_display,'三日均價大於六日均價'
+    elif pa.ckPlusGLI and pa.S1:
+      print '-X', a.Cmd_display,'量大收黑'
+    elif pa.ckPlusGLI and pa.S2:
+      print '-X', a.Cmd_display,'量縮價跌'
+    elif pa.ckPlusGLI and pa.S3:
+      print '-X', a.Cmd_display,'三日均價由上往下'
+    elif pa.ckPlusGLI and pa.S4:
+      print '-X', a.Cmd_display,'三日均價小於六日均價'
   except:
-    pass
+    print 'STOP!'
