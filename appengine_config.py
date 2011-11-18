2402b5b6046ba57cbda7a9f86928747de0700d64
Toomore Chiang <toomore0929@gmail.com>
2011-11-18 19:49:28 +0800

    Tiny change.

--- appengine_config.py
+++ appengine_config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#import os
+import os
 #os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 #from google.appengine.dist import use_library
 #use_library('django', '1.1')
