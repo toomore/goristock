a87e3c0a93bbd7e08c6245cbf64eebbeaa18b147
Toomore Chiang <toomore0929@gmail.com>
2012-02-23 23:45:19 +0800

    Add serial fetch and output csv file.

--- grs_rewrite/fetch_data.py
+++ grs_rewrite/fetch_data.py
@@ -5,11 +5,12 @@ import urllib2
 import csv
 import random
 from datetime import datetime
+from datetime import timedelta
 
-def fetch_data(stock_no, nowdatetime = datetime.today(), firsttime = 1):
+def fetch_data(stock_no, nowdatetime = datetime.today()):
     """ Fetch data from twse.com.tw
         return list.
-        從 twse.com.tw 下載資料，回傳格式為 list
+        從 twse.com.tw 下載資料，回傳格式為 csv.reader
     """
     url = ('http://www.twse.com.tw/ch/trading/exchange/'+
             'STOCK_DAY/STOCK_DAY_print.php?genpage=genpage/'+
@@ -27,3 +28,25 @@ def fetch_data(stock_no, nowdatetime = datetime.today(), firsttime = 1):
         cc_read = cc.readlines()
         csv_read = csv.reader(cc_read)
     return csv_read
+
+def to_list(csv_file):
+    """ [list] 串接每日資料 舊→新"""
+    tolist = []
+    for i in csv_file:
+        i = [ v.replace(' ','').replace(',','') for v in i]
+        tolist.append(i)
+    return tolist[2:]
+
+def serial_fetch(no,month=3):
+    """[list] 串接每月資料 舊→新"""
+    re = []
+    for i in range(month):
+        nowdatetime = datetime.today() - timedelta(30*i)
+        tolist = to_list(fetch_data(no,nowdatetime))
+        re = tolist + re
+    return re
+
+def out_putfile(fpath,csvlist):
+    """ 輸出成 CSV 檔 """
+    op = csv.writer(open(fpath,'wt'))
+    op.writerows(csvlist)
--- grs_rewrite/test.py
+++ grs_rewrite/test.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from fetch_data import fetch_data
+from fetch_data import serial_fetch
+from fetch_data import to_list
+from fetch_data import out_putfile
 import os
 
 print os.sys.argv
-
+'''
 c = fetch_data(os.sys.argv[1])
-for i in c:
-    print i
+c_read = to_list(c)
+
+print c_read
+'''
+
+## 測試串接
+s = serial_fetch(os.sys.argv[1],int(os.sys.argv[2]))
+print s
+
+## 顯示擷取的每日筆數
+print len(s)
+
+## 測試輸出CSV檔
+out_putfile('/dev/shm/f.csv',s)
